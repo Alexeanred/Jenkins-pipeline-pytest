2a7a7d48e125d0f7a695d1b3cb0352af4bd414be
Alexandered <duytien46kd@gmail.com>
2023-09-28 10:12:27 +0700

    push jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,22 @@
+// Declarative //
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building the app..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing the app..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying the app....'
+            }
+        }
+    }
+}
