8af8fa46086f0f1e6f6128de53d3c73c9b579d1a
Alexandered <duytien46kd@gmail.com>
2023-10-05 14:52:30 +0700

    push to github for testing

---
+++ test.py
@@ -0,0 +1,11 @@
+from math import add,minus
+
+def testAdd():
+    assert add(5,3) == 8
+    assert add(1,1) == 2
+    assert add(0,0) == 0
+
+def testMinus():
+    assert minus(5,3) == 2
+    assert minus(1,1) == 0
+    assert minus(0,0) == 0
